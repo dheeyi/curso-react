432dd1671b8e7ee19ff0524c9bc9ce321f13cc15
William Barra <dheeyi@gmail.com>
2022-12-05 14:07:26 -0800

    Subiendo laboratorio 1

---
+++ mod1/test2.js
@@ -0,0 +1,16 @@
+function muestraNombre() {
+	return 'William Barra';
+}
+
+console.log(muestraNombre());
+
+
+const muestraNombre2 = () => {
+	return 'William Barra';
+}
+
+console.log(muestraNombre2());
+
+const muestraNombre3 = () => 'William Barra';
+
+console.log(muestraNombre3());
